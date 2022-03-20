86c7f21488784a142b93dea9c0654c9b58cbfb08
Alex Nielsen <nielsen6655@gmail.com>
2022-03-20 21:11:22 +0100

    video autoplay+controls-download

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,7 @@
-const bars = document.getElementById('bars');
-const mobileMenu = document.getElementById('mobileMenu'); // To const variabler som bliver hentet fra HTML via getElementById
+const bars = document.getElementById('bars'); // id'et bars til min const variabel "bars"
+const mobileMenu = document.getElementById('mobileMenu'); // id'et mobileMenu til const varibal mobilemenu
 
 bars.addEventListener('click', function () {
-    mobileMenu.classList.toggle('active')     //addEventlistener i dette tilfælde "click" på const "bars" til Fuctionen på "mobileMenu" toggle som viser/fjerner   
+    mobileMenu.classList.toggle('active')     /*addEventlistener i dette tilfælde "click" på const "bars" til Fuctionen
+                                                på "mobileMenu" toggle som viser/fjerner active classen som er designet i css'en  */
 })
