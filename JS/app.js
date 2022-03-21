d1c6af284ef33b821bcf45f416121bb4a42da237
Alex Nielsen <nielsen6655@gmail.com>
2022-03-21 14:51:08 +0100

    valideret kode html css + js

--- JS/app.js
+++ JS/app.js
@@ -2,7 +2,6 @@ const bars = document.getElementById('bars'); // id'et bars til min const variab
 const mobileMenu = document.getElementById('mobileMenu'); // id'et mobileMenu til const varibal mobilemenu
 
 bars.addEventListener('click', function () {
-    mobileMenu.classList.toggle('active')     /*addEventlistener i dette tilfælde "click" på const "bars" til Fuctionen
+    mobileMenu.classList.toggle('active');     /*addEventlistener i dette tilfælde "click" på const "bars" til Fuctionen
                                                 på "mobileMenu" toggle som viser/fjerner active classen som er designet i css'en  */
-})
-
+});
