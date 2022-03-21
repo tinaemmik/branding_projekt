c881427e107fde068390b31d85bb0770a8b4451d
Alex Nielsen <nielsen6655@gmail.com>
2022-03-21 12:29:10 +0100

    Video - mute/unmute+
    SoMe ikoner hover - kontakt os font - forside margin og rettelser

--- JS/app.js
+++ JS/app.js
@@ -5,3 +5,17 @@ bars.addEventListener('click', function () {
     mobileMenu.classList.toggle('active')     /*addEventlistener i dette tilfælde "click" på const "bars" til Fuctionen
                                                 på "mobileMenu" toggle som viser/fjerner active classen som er designet i css'en  */
 })
+
+var vid = document.getElementById("myVideo");
+
+function enableMute() {
+    vid.muted = true;
+}
+
+function disableMute() {
+    vid.muted = false;
+}
+
+function checkMute() {
+    alert(vid.muted);
+} 
